28919853e2c544aadb08f157a17a75f30c74486c
KittiphongChampa <kittiphong.c@kkumail.com>
2023-12-24 04:53:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.get("/test", (req, res) => {
+    res.send("test");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+module.exports = app;
