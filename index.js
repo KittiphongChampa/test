32a6b3c53a18921563b9f93fa661f64547e3d42c
KittiphongChampa <kittiphong.c@kkumail.com>
2023-12-24 05:00:40 +0700

    'edit'

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
+require('dotenv').config()
 
 // Initialize Express
 const app = express();
+const mysql = require('mysql2')
+const dbConn = mysql.createConnection(process.env.DATABASE_URL)
 
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.get("/test", (req, res) => {
-    res.send("test");
-});
+app.get("/user", (req, res) => {
+    dbConn.query('SELECT * FROM users', function( err, results ) {
+        res.send(results)
+    })
+})
 
 // Initialize server
 app.listen(5000, () => {
