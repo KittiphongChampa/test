f783a00b5abc7b23f0ca8639db2e97e9977d6408
KittiphongChampa <kittiphong.c@kkumail.com>
2023-12-24 05:34:47 +0700

    'edit'

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 require('dotenv').config()
-
 // Initialize Express
 const app = express();
-app.use(cors());
+
+
 
 const Router = require('./routes/router')
 
+
+
+app.use(cors());
+
 // Create GET request
-// app.get("/", (req, res) => {
-//   res.send("Express on Vercel");
-// });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
 
 app.use(Router)
 
-// Initialize server
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
 
-module.exports = app;
+// module.exports = app;
