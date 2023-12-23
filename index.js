c3fbb5ae21b3b780ebf6d57f4dbdeab83aa34f8c
KittiphongChampa <kittiphong.c@kkumail.com>
2023-12-24 06:11:00 +0700

    'edit'

--- index.js
+++ index.js
@@ -3,71 +3,69 @@ const cors = require("cors");
 require('dotenv').config()
 const mysql = require('mysql2')
 const dbConn = mysql.createConnection(process.env.DATABASE_URL)
-// Initialize Express
-let app = express();
-
 const bodyParser = require('body-parser')
 const path = require('path')
+const fileUpload = require('express-fileupload');
+const https = require('https');
 const fs = require('fs');
-const fileUpload  = require('express-fileupload');
 
-// const https = require('https');
-// const fs = require('fs');
-// const privateKey = fs.readFileSync('key.pem', 'utf8');
-// const certificate = fs.readFileSync('cert.pem', 'utf8');
-// const credentials = { key: privateKey, cert: certificate };
-// const server = https.createServer(credentials, app);
-
-// const io = socketIO(server, {
-//     cors: {
-//       origin: process.env.CORS_ORIGIN || '*',
-//       methods: ['GET', 'POST'],
-//       allowedHeaders: ['Content-Type'],
-//     },
-// });
-
-// global.onlineUsers = new Map(); //สร้างตัวแปร global onlineUsers เพื่อเก็บข้อมูลผู้ใช้งานที่ออนไลน์ โดยใช้ Map ในการเก็บข้อมูลดังกล่าว
-// io.on('connection', (socket) => {
-//   global.chatSocket = socket; //กำหนด socket ที่เชื่อมต่อเข้ามาให้เป็นตัวแปร global chatSocket เพื่อใช้ในการสื่อสารกับ client อื่น ๆ ในภายหลัง
-//   socket.on("add-user", (userId) => { //รอรับเหตุการณ์ "add-user" จาก client เพื่อเพิ่มผู้ใช้งานใหม่ในรายชื่อผู้ใช้งานที่ออนไลน์ โดยใช้ userId เป็นตัวระบุ
-//     onlineUsers.set(userId, socket.id); //เพิ่มข้อมูลผู้ใช้งานใน Map onlineUsers โดยใช้ userId เป็นคีย์และ socket.id เป็นค่า
-//   });
-//   socket.on("send-msg", (data) => { //รอรับเหตุการณ์ "send-msg" จาก client เพื่อส่งข้อความไปยังผู้ใช้งานที่เป็นผู้รับ
-//     const sendUserSocket = onlineUsers.get(data.to);  //ดึงค่า socket.id ของผู้ใช้งานที่เป็นผู้รับจาก Map onlineUsers โดยใช้ userId เป็นคีย์
-//     if (sendUserSocket) {//ตรวจสอบว่ามี socket.id ของผู้ใช้งานที่เป็นผู้รับหรือไม่
-//       socket.to(sendUserSocket).emit("msg-receive", data.msg);//ส่งเหตุการณ์ "msg-receive" พร้อมกับข้อความdata.msg` ไปยังผู้รับผ่านการใช้งาน socket
-//     }
-//   });
-// });
-
+// Initialize Express
+let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-app.use(
-    fileUpload()
-);
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(fileUpload());
+
+// Socket.IO configuration
+const socketIO = require('socket.io');
+const server = https.createServer({
+    key: fs.readFileSync('key.pem', 'utf8'),
+    cert: fs.readFileSync('cert.pem', 'utf8')
+}, app);
+const io = socketIO(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN || '*',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+    },
+});
+
+// Global variable
+global.onlineUsers = new Map();
+
+// Socket.IO connection
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-receive", data.msg);
+        }
+    });
+});
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    res.send("Express on Vercel");
 });
 
+// Create GET request for users
 app.get("/user", async (req, res) => {
-    dbConn.query('SELECT * FROM users', function( err, results ) {
+    dbConn.query('SELECT * FROM users', function (err, results) {
         res.send(results)
-    })
-})
+    });
+});
 
 // Initialize server
-
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+server.listen(process.env.PORT || 5000, () => {
+    console.log("Running on port", process.env.PORT || 5000);
 });
 
 module.exports = app;
