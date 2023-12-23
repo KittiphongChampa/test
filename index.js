3d028077e6caca2dbdab26119d2189beafce8780
KittiphongChampa <kittiphong.c@kkumail.com>
2023-12-24 05:22:37 +0700

    'edit'

--- index.js
+++ index.js
@@ -6,29 +6,15 @@ const app = express();
 app.use(cors());
 
 const Router = require('./routes/router')
-const bodyParser = require('body-parser')
-const path = require('path')
-const fs = require('fs');
-const fileUpload  = require('express-fileupload');
 
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
 app.use(
     fileUpload()
 );
-
-app.use(Router)
 
 // Initialize server
 app.listen(5000, () => {
